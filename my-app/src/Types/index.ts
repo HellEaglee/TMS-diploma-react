c79fed4e07fb727aed21cee7a48edc14c4f32b9d
HellEaglee <helleagleee@gmail.com>
2022-08-14 15:29:15 +0300

    Cleaning

--- my-app/src/Types/index.ts
+++ my-app/src/Types/index.ts
@@ -28,25 +28,6 @@ export type PdfModel = {
   chapter5: string;
 };
 
-export type CartBookItemProps = {
-  error: string;
-  title: string;
-  subtitle: string;
-  authors: string;
-  publisher: string;
-  isbn10: string;
-  isbn13: string;
-  pages: string;
-  year: string;
-  rating: string;
-  desc: string;
-  price: string;
-  image: string;
-  url: string;
-  pdf: PdfModel;
-  favorite: boolean;
-};
-
 export type CartSumProps = {
   price: number;
 };
